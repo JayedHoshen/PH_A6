28f4e64d43f1713e5a379246fd5c4ce8ec2101dd
jayedHoshen <jayedkn64@gmail.com>
2026-04-15 23:37:40 +0600

    added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="btn btn-lg btn-accent">Testing</h1>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main></main>
+      <footer></footer>
     </>
   );
 }
